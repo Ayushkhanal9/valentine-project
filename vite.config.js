726d856b6a276eef9bb6ed81ec97a8565848a1c0
Ayush Khanal <aayush668@gmail.com>
2025-02-08 00:14:39 -0500

    fix add csp headers

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,11 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/valentine-project/', // 👈 Important for GitHub Pages
+  base: '/valentine-project/',
   plugins: [react()],
+  server: {
+    headers: {
+      'Content-Security-Policy': "script-src 'self' 'unsafe-inline' 'unsafe-eval';"
+    }
+  }
 });
